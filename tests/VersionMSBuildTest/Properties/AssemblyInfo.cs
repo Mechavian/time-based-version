cbba086770e2df7cab8bb08775dc1c72b3284bd4
Jason Tremper <jason@mechavian.com>
2014-09-26 15:11:15 -0400

    Adding NuGet spec and MSBuild support

--- tests/VersionMSBuildTest/Properties/AssemblyInfo.cs
+++ tests/VersionMSBuildTest/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Version")]
+[assembly: AssemblyTitle("VersionMSBuildTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Mechavian Inc.")]
-[assembly: AssemblyProduct("Version")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VersionMSBuildTest")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,10 +19,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("170426ef-ff75-446d-bf60-ba0486784da7")]
-
-[assembly: AssemblyVersion("1.1.0.*")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
-
-[assembly: InternalsVisibleTo("Version.Test")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
+[assembly: Guid("da03f8db-9836-44b0-a599-cf2d8ce53066")]
